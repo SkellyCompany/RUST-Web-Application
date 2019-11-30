f461815f94d430b015252ff21e9384981f14b5f5
schemabuoi <charyszczakg@gmail.com>
2019-11-30 16:25:23 +0100

    Create ProductModel & Product & ProductStock entities

---
+++ backend/RustWebApplication.Core.Entity/Product/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RustWebApplication.Core.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public ProductModel ProductModel { get; set; }
+        public List<ProductStock> ProductStocks { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ backend/RustWebApplication.Core.Entity/Product/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RustWebApplication.Core.Entity
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ProductType ProductType { get; set; }
+        public double Price { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
