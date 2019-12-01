e2bc325db183f5de2059cfd1c4f04f01cd9e9d0e
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-01 16:40:48 +0100

    Created vision component.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { VisionComponent } from './main/vision/vision.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterComponent
+    FooterComponent,
+    VisionComponent
   ],
   imports: [
     BrowserModule,
