83e1118a9df1abb09c8dbc001c5e501f94049ea8
schemabuoi <charyszczakg@gmail.com>
2019-12-01 18:11:13 +0100

    Implement all of the product repository interfaces

--- backend/RustWebApplication.Core/DomainService/IProductSizeRepository.cs
+++ backend/RustWebApplication.Core/DomainService/IProductSizeRepository.cs
@@ -1,7 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using RustWebApplication.Core.Entity;
+
 namespace RustWebApplication.Core.DomainService
 {
     public interface IProductSizeRepository
     {
+        ProductSize Create(ProductSize newProductSize);
+
+        ProductSize Read(int productSizeId);
+
+        IEnumerable<ProductSize> ReadAll();
+
+        ProductSize Update(ProductSize updatedProductSize);
+
+        ProductSize Delete(int productSizeId);
     }
 }
