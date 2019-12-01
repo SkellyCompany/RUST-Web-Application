650b2681029b4180226c2483636b28ea2e7b8345
Kiddo <Davidkalatzis@gmail.com>
2019-12-01 19:29:39 +0100

    Update CountryService.cs

--- backend/RustWebApplication.Core/ApplicationService/Services/CountryService.cs
+++ backend/RustWebApplication.Core/ApplicationService/Services/CountryService.cs
@@ -7,8 +7,8 @@ namespace RustWebApplication.Core.ApplicationService.Services
 {
     public class CountryService : ICountryService
     {
-
         private readonly ICountryRepository _countryRepository;
+
 
         public CountryService(ICountryRepository countryRepository)
         {
