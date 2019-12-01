d60cac5e7e829e3694ee3896a5c1af1523426ce6
schemabuoi <charyszczakg@gmail.com>
2019-12-01 19:23:27 +0100

    Create empty services, ready for creating tests

---
+++ backend/RustWebApplication.Core/ApplicationService/Services/ProductTypeService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using RustWebApplication.Core.DomainService;
+using RustWebApplication.Core.Entity;
+
+namespace RustWebApplication.Core.ApplicationService.Services
+{
+    public class ProductTypeService : IProductTypeService
+    {
+        private readonly IProductTypeRepository _productTypeRepository;
+
+        public ProductTypeService(IProductTypeRepository productTypeRepository)
+        {
+            _productTypeRepository = productTypeRepository;
+        }
+
+        public ProductType Create(ProductType newProductType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductType Delete(int productTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductType Read(int productTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductType> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductType Update(ProductType updatedProductType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
