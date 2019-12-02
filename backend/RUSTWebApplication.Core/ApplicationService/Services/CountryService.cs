739d61c7445f7ca909068bd453bb58214c8fb36b
Kevin Luu - I17 <kevin.luu@nordakademie.de>
2019-12-02 12:16:22 +0100

    minor changes

--- backend/RUSTWebApplication.Core/ApplicationService/Services/CountryService.cs
+++ backend/RUSTWebApplication.Core/ApplicationService/Services/CountryService.cs
@@ -1,43 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using RUSTWebApplication.Core.Entity.Order;
 using RUSTWebApplication.Core.DomainService;
 
 namespace RUSTWebApplication.Core.ApplicationService.Services
 {
-	public class CountryService : ICountryService
-	{
-		private readonly ICountryRepository _countryRepository;
+    public class CountryService : ICountryService
+    {
+        private readonly ICountryRepository _countryRepository;
 
 
-		public CountryService(ICountryRepository countryRepository)
-		{
-			_countryRepository = countryRepository;
-		}
+        public CountryService(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
 
-		public Country Create(Country newCountry)
-		{
-			throw new NotImplementedException();
-		}
+        public Country Create(Country newCountry)
+        {
+            return _countryRepository.Create(newCountry);
+        }
 
-		public Country Delete(int countryId)
-		{
-			throw new NotImplementedException();
-		}
+        public Country Delete(int countryId)
+        {
+            return _countryRepository.Delete(countryId);
+        }
 
-		public Country Read(int countryId)
-		{
-			throw new NotImplementedException();
-		}
+        public Country Read(int countryId)
+        {
+            return _countryRepository.Read(countryId);
+        }
 
-		public List<Country> ReadAll()
-		{
-			throw new NotImplementedException();
-		}
+        public List<Country> ReadAll()
+        {
+            return _countryRepository.ReadAll().ToList();
+        }
 
-		public Country Update(Country updatedCountry)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public Country Update(Country updatedCountry)
+        {
+            return _countryRepository.Update(updatedCountry);
+        }
+
+    }
 }
