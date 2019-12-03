596e245e163dd58c1c7a01c8bea3bcaa6ca6cfa9
Kevin Luu - I17 <kevin.luu@nordakademie.de>
2019-12-03 13:36:58 +0100

    added exceptions to countryservice

--- backend/RUSTWebApplication.Core/ApplicationService/Services/CountryService.cs
+++ backend/RUSTWebApplication.Core/ApplicationService/Services/CountryService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using RUSTWebApplication.Core.Entity.Order;
 using RUSTWebApplication.Core.DomainService;
 
@@ -18,6 +20,7 @@ namespace RUSTWebApplication.Core.ApplicationService.Services
 
         public Country Create(Country newCountry)
         {
+            ValidateCreateCountry(newCountry);
             return _countryRepository.Create(newCountry);
         }
 
@@ -33,12 +36,53 @@ namespace RUSTWebApplication.Core.ApplicationService.Services
 
         public Country Update(Country updatedCountry)
         {
+            ValidateUpdateCountry(updatedCountry);
             return _countryRepository.Update(updatedCountry);
         }
 
         public Country Delete(int countryId)
         {
             return _countryRepository.Delete(countryId);
+        }
+
+        /*   private void ValidateCountryId(int countryId)
+           {
+                if (countryId < 1)
+                {
+                    throw new InvalidDataException("ID must be over zero.");
+                } 
+
+            } */
+
+        private void ValidateCountryName(Country country)
+        {
+            if (string.IsNullOrEmpty(country.Name))
+            {
+                throw new InvalidDataException("You need to specify a name for the Country.");
+            }
+
+            if (char.IsUpper(country.Name[0]))
+            {
+                throw new InvalidDataContractException("The countries name must start with an uppercase letter.");
+            }
+        }
+
+        private void ValidateCreateCountry(Country country)
+        {
+            if (country.Id != default)
+            {
+                throw new InvalidDataException("You are not allowed to specify an ID when creating a country.");
+            }
+            ValidateCountryName(country);
+        }
+
+        private void ValidateUpdateCountry(Country country)
+        {
+            if (_countryRepository.Read(country.Id) == null)
+            {
+                throw new InvalidDataException($"Cannot find a country with an ID: {country.Id}");
+            }
+            ValidateCountryName(country);
         }
 
     }
