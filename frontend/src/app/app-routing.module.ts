ece5546e988c11233dc4ec7b7b8c6d4f42f87ef9
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-04 00:41:47 +0100

    Added routing for credits component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { VisionComponent } from './main/vision/vision.component';
 import { ProductListComponent } from './main/shop/product-list/product-list.component';
 import { ProductDetailsComponent } from './main/shop/product-details/product-details.component';
+import { CreditsComponent } from './main/credits/credits.component';
 
 
 const routes: Routes = [
   { path: 'vision', component: VisionComponent },
   { path: 'shop', component: ProductListComponent },
   { path: 'shop/:id', component: ProductDetailsComponent },
+  { path: 'credits', component: CreditsComponent },
 ];
 
 @NgModule({
