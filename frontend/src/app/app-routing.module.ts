ffeb97d3858eec4080cd86164124f2af3b52a04d
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-03 09:32:42 +0100

    Added routing for product-list component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VisionComponent } from './main/vision/vision.component';
+import { ProductListComponent } from './main/shop/product-list/product-list.component';
 
 
 const routes: Routes = [
   { path: 'vision', component: VisionComponent },
+  { path: 'shop', component: ProductListComponent },
 ];
 
 @NgModule({
