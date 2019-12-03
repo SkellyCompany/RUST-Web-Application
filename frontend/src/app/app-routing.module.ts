e4dffbd3e049c0b6e61adb59be08c3e95ca6064d
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-03 12:30:46 +0100

    Added routing for product-details component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VisionComponent } from './main/vision/vision.component';
 import { ProductListComponent } from './main/shop/product-list/product-list.component';
+import { ProductDetailsComponent } from './main/shop/product-details/product-details.component';
 
 
 const routes: Routes = [
   { path: 'vision', component: VisionComponent },
   { path: 'shop', component: ProductListComponent },
+  { path: 'shop/:id', component: ProductDetailsComponent },
 ];
 
 @NgModule({
