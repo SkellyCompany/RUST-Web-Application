f08b2babf8d3ffa42d5a0b12b97549adff86fb27
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-03 09:24:56 +0100

    Added product-list component.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { VisionComponent } from './main/vision/vision.component';
+import { ProductListComponent } from './main/shop/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
     FooterComponent,
-    VisionComponent
+    VisionComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
