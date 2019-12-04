a518e4cfc78039f13895faa8c91f7c41f02c4f97
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-04 10:24:51 +0100

    Added routing for checkout component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { VisionComponent } from './main/vision/vision.component';
 import { ProductListComponent } from './main/shop/product-list/product-list.component';
 import { ProductDetailsComponent } from './main/shop/product-details/product-details.component';
 import { CreditsComponent } from './main/credits/credits.component';
+import { CheckoutComponent } from './main/checkout/checkout.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'shop', component: ProductListComponent },
   { path: 'shop/:id', component: ProductDetailsComponent },
   { path: 'credits', component: CreditsComponent },
+  { path: 'checkout', component: CheckoutComponent},
 ];
 
 @NgModule({
