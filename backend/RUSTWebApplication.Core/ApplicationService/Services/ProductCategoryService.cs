bcfe11daf49d9195f63f84f4b33192a646c7312b
Kevin Luu - I17 <kevin.luu@nordakademie.de>
2019-12-04 10:48:28 +0100

    added exception in order to pass test for productcategoryservice

--- backend/RUSTWebApplication.Core/ApplicationService/Services/ProductCategoryService.cs
+++ backend/RUSTWebApplication.Core/ApplicationService/Services/ProductCategoryService.cs
@@ -18,6 +18,7 @@ namespace RUSTWebApplication.Core.ApplicationService.Services
 
 		public ProductCategory Create(ProductCategory newProductCategory)
         {
+            ValidateCreate(newProductCategory);
             return _productCategoryRepository.Create(newProductCategory);
         }
 
@@ -33,12 +34,49 @@ namespace RUSTWebApplication.Core.ApplicationService.Services
 
 		public ProductCategory Update(ProductCategory updatedProductCategory)
         {
+            ValidateUpdate(updatedProductCategory);
             return _productCategoryRepository.Update(updatedProductCategory);
         }
 
         public ProductCategory Delete(int productCategoryId)
         {
             return _productCategoryRepository.Delete(productCategoryId);
+        }
+        private void ValidateCreate(ProductCategory productCategory)
+        {
+            ValidateNull(productCategory);
+            if (productCategory.Id != default)
+            {
+                throw new ArgumentException("You are not allowed to specify an ID when creating a product category.");
+            }
+            ValidateName(productCategory);
+        }
+
+        private void ValidateUpdate(ProductCategory productCategory)
+        {
+            ValidateNull(productCategory);
+            if (_productCategoryRepository.Read(productCategory.Id) == null)
+            {
+                throw new ArgumentException($"Cannot find a product category with an ID: {productCategory.Id}");
+            }
+            ValidateName(productCategory);
+        }
+
+
+        private void ValidateNull(ProductCategory productCategory)
+        {
+            if (productCategory == null)
+            {
+                throw new ArgumentNullException("Product category is null");
+            }
+        }
+
+        private void ValidateName(ProductCategory productCategory)
+        {
+            if (string.IsNullOrEmpty(productCategory.Name))
+            {
+                throw new ArgumentException("You need to specify a name for the product category.");
+            }
         }
     }
 }
