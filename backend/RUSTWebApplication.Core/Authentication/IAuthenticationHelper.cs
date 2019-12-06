4a009bee4b29e9d3de95c7a352add9c9a7140d51
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-06 16:33:15 +0100

    Added authentication interface.

---
+++ backend/RUSTWebApplication.Core/Authentication/IAuthenticationHelper.cs
@@ -0,0 +1,11 @@
+﻿using RUSTWebApplication.Core.Entity.Authentication;
+
+namespace RUSTWebApplication.Core.Authentication
+{
+	interface IAuthenticationHelper
+	{
+		void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+		bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
+		string GenerateToken(User user);
+	}
+}
