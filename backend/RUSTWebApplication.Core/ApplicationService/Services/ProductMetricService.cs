d2efdb686dc840407631fa2526dd25fe853dafdc
Kevin Luu - I17 <kevin.luu@nordakademie.de>
2019-12-07 16:13:56 +0100

    added exception to productMetricService in order to pass test

--- backend/RUSTWebApplication.Core/ApplicationService/Services/ProductMetricService.cs
+++ backend/RUSTWebApplication.Core/ApplicationService/Services/ProductMetricService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using RUSTWebApplication.Core.DomainService;
 using RUSTWebApplication.Core.Entity.Product;
@@ -20,27 +21,109 @@ namespace RUSTWebApplication.Core.ApplicationService.Services
         
         public ProductMetric Create(ProductMetric newProductMetric)
         {
-            throw new System.NotImplementedException();
+            ValidateCreate(newProductMetric);
+            return _productMetricRepository.Create(newProductMetric);
         }
 
         public ProductMetric Read(int productMetricId)
         {
-            throw new System.NotImplementedException();
+            return _productMetricRepository.Read(productMetricId);
         }
 
         public List<ProductMetric> ReadAll()
         {
-            throw new System.NotImplementedException();
+            return _productMetricRepository.ReadAll();
         }
 
         public ProductMetric Update(ProductMetric updatedProductMetric)
         {
-            throw new System.NotImplementedException();
+            ValidateUpdate(updatedProductMetric);
+            return _productMetricRepository.Update(updatedProductMetric);
         }
 
         public ProductMetric Delete(int productMetricId)
         {
-            throw new System.NotImplementedException();
+            return _productMetricRepository.Delete(productMetricId);
+        }
+        private void ValidateCreate(ProductMetric productMetric)
+        {
+            ValidateNull(productMetric);
+            if (productMetric.Id != default)
+            {
+                throw new ArgumentException("You are not allowed to specify an ID when creating a Product Metric.");
+            }
+            ValidateName(productMetric);
+            ValidateProductModel(productMetric);
+            ValidateMetricXValue(productMetric);
+            ValidateMetricYValue(productMetric);
+            ValidateMetricZValue(productMetric);
+        }
+
+        private void ValidateUpdate(ProductMetric productMetric)
+        {
+            ValidateNull(productMetric);
+            if (_productMetricRepository.Read(productMetric.Id) == null)
+            {
+                throw new ArgumentException($"Cannot find a Product Metric with an ID: {productMetric.Id}");
+            }
+            ValidateName(productMetric);
+            ValidateProductModel(productMetric);
+            ValidateMetricXValue(productMetric);
+            ValidateMetricYValue(productMetric);
+            ValidateMetricZValue(productMetric);
+
+        }
+
+        private void ValidateNull(ProductMetric productSize)
+        {
+            if (productSize == null)
+            {
+                throw new ArgumentNullException("Product Metric is null");
+            }
+        }
+
+        private void ValidateName(ProductMetric productMetric)
+        {
+            if (string.IsNullOrEmpty(productMetric.Name))
+            {
+                throw new ArgumentException("Name can not be empty.");
+            }
+        }
+        private void ValidateProductModel(ProductMetric productMetric)
+        {
+            if (productMetric.ProductModel == null)
+            {
+                throw new ArgumentException("You need to specify a Product Model for the Product Metric.");
+            }
+
+            if (_productModelRepository.Read(productMetric.ProductModel.Id) == null)
+            {
+                throw new ArgumentException($"Product Model with the ID: {productMetric.ProductModel.Id} doesn't exist.'");
+            }
+        }
+
+        private void ValidateMetricXValue(ProductMetric productMetric)
+        {
+            if (string.IsNullOrEmpty(productMetric.MetricX))
+            {
+                throw new ArgumentException("MetricX can not be empty");
+            }
+        }
+
+        private void ValidateMetricYValue(ProductMetric productMetric)
+        {
+            if (string.IsNullOrEmpty(productMetric.MetricY))
+            {
+                throw new ArgumentException("MetricY can not be empty");
+            }
+        }
+
+        private void ValidateMetricZValue(ProductMetric productMetric)
+        {
+            if (string.IsNullOrEmpty(productMetric.MetricZ))
+            {
+                throw new ArgumentException("MetricZ can not be empty");
+            }
         }
     }
 }
