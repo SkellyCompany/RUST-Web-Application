d6ba1b88a0aca8a8ef0cd5e54ce0d2704d66167e
schemabuoi <charyszczakg@gmail.com>
2019-12-10 11:53:57 +0100

    Implement context and country repository

--- backend/RUSTWebApplication.Infrastructure/RUSTWebApplicationContext.cs
+++ backend/RUSTWebApplication.Infrastructure/RUSTWebApplicationContext.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RUSTWebApplication.Core.Entity.Authentication;
+using RUSTWebApplication.Core.Entity.Order;
+using RUSTWebApplication.Core.Entity.Product;
 
 namespace RUSTWebApplication.Infrastructure
 {
-    public class RUSTWebApplicationContext
+    public class RUSTWebApplicationContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<ProductModel> ProductModels { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductStock> ProductStocks { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductMetric> ProductMetrics { get; set; }
+        public DbSet<ProductSize> ProductSizes { get; set; }
+
+        public RUSTWebApplicationContext(DbContextOptions<RUSTWebApplicationContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+
+        }
+
     }
 }
+
+    
--- backend/RUSTWebApplication.Infrastructure/Repositories/CountryRepository.cs
+++ backend/RUSTWebApplication.Infrastructure/Repositories/CountryRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using RUSTWebApplication.Core.DomainService;
 using RUSTWebApplication.Core.Entity.Order;
 
@@ -6,29 +8,44 @@ namespace RUSTWebApplication.Infrastructure.Repositories
 {
     public class CountryRepository: ICountryRepository
     {
+
+        private readonly RUSTWebApplicationContext _ctx;
+
+        public CountryRepository(RUSTWebApplicationContext ctx)
+        {
+            _ctx = ctx;
+        }
+
         public Country Create(Country newCountry)
         {
-            throw new System.NotImplementedException();
+            _ctx.Countries.Attach(newCountry).State = EntityState.Added;
+            _ctx.SaveChanges();
+            return newCountry;
         }
 
         public Country Read(int countryId)
         {
-            throw new System.NotImplementedException();
+            return _ctx.Countries.AsNoTracking().FirstOrDefault(c => c.Id == countryId);
         }
 
         public IEnumerable<Country> ReadAll()
         {
-            throw new System.NotImplementedException();
+            return _ctx.Countries.AsNoTracking();
         }
 
         public Country Update(Country updatedCountry)
         {
-            throw new System.NotImplementedException();
+            _ctx.Countries.Attach(updatedCountry).State = EntityState.Modified;
+            _ctx.SaveChanges();
+            return updatedCountry;
         }
 
         public Country Delete(int countryId)
         {
-            throw new System.NotImplementedException();
+            var countryToDelete = _ctx.Countries.FirstOrDefault(c => c.Id == countryId);
+            _ctx.Countries.Remove(countryToDelete);
+            _ctx.SaveChanges();
+            return countryToDelete;
         }
     }
 }
