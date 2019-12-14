b9148adb56683320002125004cbc2a6b6f3d94af
kidagine <36992613+kidagine@users.noreply.github.com>
2019-12-14 03:41:22 +0100

    Initial setup for adding real data to the cart.

---
+++ frontend/src/app/shared/models/productCart.ts
@@ -0,0 +1,8 @@
+export class ProductCart {
+  id?: number;
+  name: string;
+  price: number;
+  color:string;
+  imagePath: string;
+  quantity: number;
+}
--- frontend/src/app/shared/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ProductCart } from '../models/productCart';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,12 +8,40 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  @Input() isCartVisible: boolean = false;
+  isCartVisible: boolean = false;
+  productCarts: ProductCart[] = [];
 
   constructor() {
    }
 
   ngOnInit() {
+  }
+
+  addProductToCart(productCart: ProductCart){
+    console.log(productCart.name);
+    this.productCarts.push(productCart);
+  }
+
+  setSubtotal(){
+    let subtotal;
+    for (var i = 0; i < this.productCarts.length; i++) {
+      subtotal = this.productCarts[i].price;
+    }
+    console.log(subtotal);
+    return subtotal;
+  }
+
+  setProductCartQuantity(number: number){
+    if (number > 0){
+      if (this.productCarts[0].quantity < 10){
+        this.productCarts[0].quantity += number;
+      }
+    }
+    else  if (number < 0){
+      if (this.productCarts[0].quantity = 1){
+        this.productCarts.splice(0, 1);
+      }
+    }
   }
 
   setCartVisibility(){
